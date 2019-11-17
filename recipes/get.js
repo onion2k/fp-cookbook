0c3c856463077435796592b23355b6be049f61e4
Chris Neale <chris@usablehq.com>
2019-11-17 15:59:04 +0000

    Added getor and get curried

--- recipes/get.js
+++ recipes/get.js
@@ -1,8 +1,22 @@
 const fp = require('lodash/fp');
-const libraries = require('../libraries.json')
+const libraries = require('../libraries.json');
 
-const { get } = fp;
+const { get, getOr } = fp;
 
-const library = get('libraries[0].name', libraries)
+/**
+ * get returns a value based on a path
+ */
+const name = get('libraries[0].name', libraries);
+console.log(name);
 
-console.log(library)
+/**
+ * getOr can return a value if the fetch is falsy
+ */
+const telephone = getOr('No Telephone', 'libraries[0].telephone', libraries);
+console.log(telephone);
+
+/**
+ * fp functions are curried
+ */
+const address = get('libraries[0].address');
+console.log(address(libraries));
